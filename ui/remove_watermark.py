4180d784ba5a23469d1d73a1cc4881a8bb11489b
sfmoraa <sfx-sjtu@sjtu.edu.cn>
2024-04-07 16:26:46 +0800

    jinjin

--- ui/remove_watermark.py
+++ ui/remove_watermark.py
@@ -10,7 +10,12 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import QUrl
 import cv2
+import os
+from tqdm import tqdm
 
+import glob
+import numpy as np
+from moviepy.editor import VideoFileClip
 from qfluentwidgets import Action, FluentIcon
 from qfluentwidgets.multimedia import VideoWidget
 
@@ -148,106 +153,107 @@ class remove_watermark(Ui_remove, QWidget):
             self.opencv_cap.set(cv2.CAP_PROP_POS_FRAMES, self.frame_index)
             self.__show_img(self.opencv_cap.read()[1])
 
-def __remove_watermark(self):
-    def ensure_directory_exists(directory):
-        if not os.path.exists(directory):
+    def __remove_watermark(self):
+        def ensure_directory_exists(directory):
+            if not os.path.exists(directory):
+                try:
+                    os.makedirs(directory)
+                    print(f"创建目录: {directory}")
+                except OSError as error:
+                    print(f"创建目录 {directory} 时出错: {error}")
+                    raise
+
+        def is_valid_video_file(file):
             try:
-                os.makedirs(directory)
-                print(f"创建目录: {directory}")
-            except OSError as error:
-                print(f"创建目录 {directory} 时出错: {error}")
-                raise
+                with VideoFileClip(file) as video_clip:
+                    return True
+            except Exception as e:
+                print(f"无效的视频文件: {file}, 错误: {e}")
+                return False
 
-    def is_valid_video_file(file):
-        try:
-            with VideoFileClip(file) as video_clip:
-                return True
-        except Exception as e:
-            print(f"无效的视频文件: {file}, 错误: {e}")
-            return False
+        def get_first_valid_frame(video_clip, threshold=10, num_frames=10):
+            total_frames = int(video_clip.fps * video_clip.duration)
+            frame_indices = [int(i * total_frames / num_frames) for i in range(num_frames)]
 
-    def get_first_valid_frame(video_clip, threshold=10, num_frames=10):
-        total_frames = int(video_clip.fps * video_clip.duration)
-        frame_indices = [int(i * total_frames / num_frames) for i in range(num_frames)]
+            for idx in frame_indices:
+                frame = video_clip.get_frame(idx / video_clip.fps)
+                if frame.mean() > threshold:
+                    return frame
 
-        for idx in frame_indices:
-            frame = video_clip.get_frame(idx / video_clip.fps)
-            if frame.mean() > threshold:
-                return frame
+            return video_clip.get_frame(0)
 
-        return video_clip.get_frame(0)
+        def select_roi_for_mask(video_clip):
+            frame = get_first_valid_frame(video_clip)
 
-    def select_roi_for_mask(video_clip):
-        frame = get_first_valid_frame(video_clip)
+            # 将视频帧调整为720p显示
+            display_height = 720
+            scale_factor = display_height / frame.shape[0]
+            display_width = int(frame.shape[1] * scale_factor)
+            display_frame = cv2.resize(frame, (display_width, display_height))
+            display_frame = cv2.cvtColor(display_frame, cv2.COLOR_BGR2RGB)
 
-        # 将视频帧调整为720p显示
-        display_height = 720
-        scale_factor = display_height / frame.shape[0]
-        display_width = int(frame.shape[1] * scale_factor)
-        display_frame = cv2.resize(frame, (display_width, display_height))
+            # instructions = ""
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            # cv2.putText(display_frame, instructions, (10, 30), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-        instructions = "选择ROI区域并按空格或回车键"
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(display_frame, instructions, (10, 30), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            r = cv2.selectROI(display_frame)
+            cv2.destroyAllWindows()
 
-        r = cv2.selectROI(display_frame)
-        cv2.destroyAllWindows()
+            r_original = (
+                int(r[0] / scale_factor), int(r[1] / scale_factor), int(r[2] / scale_factor), int(r[3] / scale_factor))
 
-        r_original = (
-            int(r[0] / scale_factor), int(r[1] / scale_factor), int(r[2] / scale_factor), int(r[3] / scale_factor))
+            return r_original
 
-        return r_original
+        def detect_watermark_adaptive(frame, roi):
+            roi_frame = frame[roi[1]:roi[1] + roi[3], roi[0]:roi[0] + roi[2]]
+            gray_frame = cv2.cvtColor(roi_frame, cv2.COLOR_BGR2GRAY)
+            _, binary_frame = cv2.threshold(gray_frame, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-    def detect_watermark_adaptive(frame, roi):
-        roi_frame = frame[roi[1]:roi[1] + roi[3], roi[0]:roi[0] + roi[2]]
-        gray_frame = cv2.cvtColor(roi_frame, cv2.COLOR_BGR2GRAY)
-        _, binary_frame = cv2.threshold(gray_frame, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+            mask = np.zeros_like(frame[:, :, 0], dtype=np.uint8)
+            mask[roi[1]:roi[1] + roi[3], roi[0]:roi[0] + roi[2]] = binary_frame
 
-        mask = np.zeros_like(frame[:, :, 0], dtype=np.uint8)
-        mask[roi[1]:roi[1] + roi[3], roi[0]:roi[0] + roi[2]] = binary_frame
+            return mask
 
-        return mask
+        def generate_watermark_mask(video_clip, num_frames=10, min_frame_count=7):
+            total_frames = int(video_clip.duration * video_clip.fps)
+            frame_indices = [int(i * total_frames / num_frames) for i in range(num_frames)]
 
-    def generate_watermark_mask(video_clip, num_frames=10, min_frame_count=7):
-        total_frames = int(video_clip.duration * video_clip.fps)
-        frame_indices = [int(i * total_frames / num_frames) for i in range(num_frames)]
+            frames = [video_clip.get_frame(idx / video_clip.fps) for idx in frame_indices]
+            r_original = select_roi_for_mask(video_clip)
 
-        frames = [video_clip.get_frame(idx / video_clip.fps) for idx in frame_indices]
-        r_original = select_roi_for_mask(video_clip)
+            masks = [detect_watermark_adaptive(frame, r_original) for frame in frames]
 
-        masks = [detect_watermark_adaptive(frame, r_original) for frame in frames]
+            final_mask = sum((mask == 255).astype(np.uint8) for mask in masks)
+            # 根据像素点在至少min_frame_count张以上的帧中的出现来生成最终的遮罩
+            final_mask = np.where(final_mask >= min_frame_count, 255, 0).astype(np.uint8)
 
-        final_mask = sum((mask == 255).astype(np.uint8) for mask in masks)
-        # 根据像素点在至少min_frame_count张以上的帧中的出现来生成最终的遮罩
-        final_mask = np.where(final_mask >= min_frame_count, 255, 0).astype(np.uint8)
+            kernel = np.ones((5, 5), np.uint8)
+            return cv2.dilate(final_mask, kernel)
 
-        kernel = np.ones((5, 5), np.uint8)
-        return cv2.dilate(final_mask, kernel)
+        def process_video(video_clip, output_path, apply_mask_func):
+            total_frames = int(video_clip.duration * video_clip.fps)
+            progress_bar = tqdm(total=total_frames, desc="正在处理帧", unit="帧")
 
-    def process_video(video_clip, output_path, apply_mask_func):
-        total_frames = int(video_clip.duration * video_clip.fps)
-        progress_bar = tqdm(total=total_frames, desc="正在处理帧", unit="帧")
+            def process_frame(frame):
+                result = apply_mask_func(frame)
+                progress_bar.update(1000)
+                return result
 
-        def process_frame(frame):
-            result = apply_mask_func(frame)
-            progress_bar.update(1000)
-            return result
+            processed_video = video_clip.fl_image(process_frame, apply_to=["each"])
+            processed_video.write_videofile(f"{output_path}.mp4", codec="libx264")
 
-        processed_video = video_clip.fl_image(process_frame, apply_to=["each"])
-        processed_video.write_videofile(f"{output_path}.mp4", codec="libx264")
+        # 主代码
+        output_dir = "output"
+        ensure_directory_exists(output_dir)
 
-    # 主代码
-    output_dir = "output"
-    ensure_directory_exists(output_dir)
+        if is_valid_video_file(self.file_name):
+            video_clip = VideoFileClip(self.file_name)
+            watermark_mask = generate_watermark_mask(video_clip)
 
-    if is_valid_video_file(self.file_name):
-        video_clip = VideoFileClip(self.file_name)
-        watermark_mask = generate_watermark_mask(video_clip)
-
-        mask_func = lambda frame: cv2.inpaint(frame, watermark_mask, 3, cv2.INPAINT_NS)
-        video_name = os.path.basename(self.file_name)
-        output_video_path = os.path.join(output_dir, os.path.splitext(video_name)[0])
-        process_video(video_clip, output_video_path, mask_func)
-        print(f"成功处理视频文件: {video_name}")
-    else:
-        print("无效的视频文件")
+            mask_func = lambda frame: cv2.inpaint(frame, watermark_mask, 3, cv2.INPAINT_NS)
+            video_name = os.path.basename(self.file_name)
+            output_video_path = os.path.join(output_dir, os.path.splitext(video_name)[0])
+            process_video(video_clip, output_video_path, mask_func)
+            print(f"成功处理视频文件: {video_name}")
+        else:
+            print("无效的视频文件")
